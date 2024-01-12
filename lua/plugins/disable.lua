8429f963f40ff5420559a1bc450df4c279215ad6
Harrys Kedjnane <harrys.kedjnane@epita.fr>
2024-01-12 12:28:51 +0100

    disable mason

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,3 +1,6 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false }
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false }
+  -- { "neovim/nvim-lspconfig", enabled = false }, --require("lazy-nix-helper").mason_enabled() },
+  { "williamboman/mason.nvim", enabled = false }, --require("lazy-nix-helper").mason_enabled() },
+  { "williamboman/mason-lspconfig.nvim", enabled = false } --require("lazy-nix-helper").mason_enabled() }
 }
